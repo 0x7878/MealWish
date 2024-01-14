f00067ae8bda0d0bcb65103af38b180740de86b2
Maurice Schmicking <17197791+0x7878@users.noreply.github.com>
2024-01-14 16:57:54 +0000

    add navigation to move between calender weeks

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,106 @@
-import React from 'react';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { HassContext } from './HassContext';
+import React from "react";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { HassContext } from "./HassContext";
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
-import MenuAppBar from './layout/menu-app-bar';
-import MealCard from './components/meal-card';
+import MenuAppBar from "./layout/menu-app-bar";
+import MealCard from "./components/meal-card";
 
-import Container from '@mui/material/Container';
-import { Card, CardContent, Typography } from '@mui/material';
-import api_url from './config';
+import Container from "@mui/material/Container";
+import api_url from "./config";
+import { Typography } from "@mui/material";
 
 function App() {
-  const [user, setUser] = React.useState<any>({ name: "loading..."});
+  const [user, setUser] = React.useState<any>({ name: "loading..." });
   const [meals, setMeals] = React.useState<any[]>([]);
   const eventEmitter = React.useContext(HassContext);
 
- //get all data from plan for current week
- React.useEffect(() => {
-  async function fetchData() {
-    const today = new Date() as any;
-    const currentWeek = today.getWeek();
-    const year = today.getFullYear();
-    // const endpoint = api_url + "plan?year=" + year + "&cw=" + currentWeek;
-    const endpoint = api_url + "meals_in_plan?_embed=meal&_embed=plan&plan.year=" + year + "&plan.cw=" + currentWeek;
-    const data = await fetch(endpoint).then(response => response.json());
-    setMeals(data);
-  }
-  fetchData();
+  const [cwDate, setCwDate] = React.useState<{
+    cw: number;
+    year: number;
+  } | null>();
 
- }, []); 
+  //get all data from plan for current week
+  React.useEffect(() => {
+    async function fetchData() {
+      if (!cwDate) return;
+      const endpoint =
+        api_url +
+        "meals_in_plan?_embed=meal&_embed=plan&plan.year=" +
+        cwDate.year +
+        "&plan.cw=" +
+        cwDate.cw;
+      const data = await fetch(endpoint).then((response) => response.json());
+      setMeals(data);
+    }
+    fetchData();
+  }, [cwDate]);
 
   React.useEffect(() => {
     const handleUserChanged = (hass: any) => {
       if (user.name !== hass.user.name) {
         setUser(hass.user);
       }
-    }
+    };
 
-    eventEmitter.on('hassChanged', handleUserChanged);
+    eventEmitter.on("hassChanged", handleUserChanged);
 
     return () => {
-      eventEmitter.off('hassChanged', handleUserChanged);
+      eventEmitter.off("hassChanged", handleUserChanged);
     };
   }, [eventEmitter, user]);
-console.info("render app")
-console.dir(meals)
+
+  const cwDateChanged = (cwDate: { cw: number; year: number }) => {
+    setCwDate(cwDate);
+  };
+
   return (
     <>
-      <MenuAppBar />
-      <Box style={{ overflow: 'auto', height: 'calc(100vh - 57px)', marginTop: "56px" }} sx={{ bgcolor: 'background.paper', }}>
+      <MenuAppBar cwDateChanged={cwDateChanged} />
+      <Box
+        style={{
+          overflow: "auto",
+          height: "calc(100vh - 56px)",
+          marginTop: "56px",
+        }}
+        sx={{ bgcolor: "background.paper" }}
+      >
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
           <Grid container spacing={2}>
-           {meals && meals.map((entry: any, index: number) => {
-              return (
-                <Grid item xs={12} md={4} key={index}>
-                  <MealCard user={entry.added_by} meal={entry.meal.name} image={entry.meal.image_url} />
-                </Grid>
-              )
-            }
-           )}  
+            {meals &&
+              meals.map((entry: any, index: number) => {
+                return (
+                  <Grid item xs={12} md={4} key={index}>
+                    <MealCard
+                      user={entry.added_by}
+                      meal={entry.meal.name}
+                      image={entry.meal.image_url}
+                    />
+                  </Grid>
+                );
+              })}
+            {meals.length === 0 && (
+              <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{ minHeight: '80vh' }}
+            >
+              <Grid item xs={3}>
+             <Typography variant="h3" component="div" sx={{ flexGrow: 1, color: "grey"}}>
+                No meals found for this week
+              </Typography> 
+              </Grid>
+            </Grid>
+            )}
           </Grid>
         </Container>
       </Box>
--- src/layout/menu-app-bar.tsx
+++ src/layout/menu-app-bar.tsx
@@ -1,27 +1,38 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import Divider from '@mui/material/Divider';
-import { HassContext } from '../HassContext';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import Divider from "@mui/material/Divider";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { HassContext } from "../HassContext";
+import { Box, Button } from "@mui/material";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'ha-menu-button': any;
+      "ha-menu-button": any;
     }
   }
 }
 
-export default function MenuAppBar() {
+export default function MenuAppBar(props: any) {
+
+  const {cwDateChanged} = props;
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  
+
   const ref = React.useRef<any>(null);
   const narrowRef = React.useRef<any>(undefined);
+
+  const today = new Date() as any;
+  const currentWeek = today.getWeek();
+  const year = today.getFullYear();
+  const [cwDate, setCwDate] = React.useState<{cw: number, year: number}>({cw: currentWeek, year: year});
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -31,67 +42,118 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   };
 
-const eventEmitter = React.useContext(HassContext);
+  const eventEmitter = React.useContext(HassContext);
+
+  // trigger once on load
+  React.useEffect(() => {
+    changeWeek(0);
+  }, []);
 
   React.useEffect(() => {
     const handlehassChanged = (hass: any) => {
-     ref.current.hass = hass;
-    }
-    
+      ref.current.hass = hass;
+    };
+
     const handlenarrowChanged = (narrow: any) => {
       ref.current.narrow = narrow;
-    }
+    };
 
-    eventEmitter.on('hassChanged', handlehassChanged);
-    eventEmitter.on('narrowChanged', handlenarrowChanged);
+    eventEmitter.on("hassChanged", handlehassChanged);
+    eventEmitter.on("narrowChanged", handlenarrowChanged);
 
     return () => {
-      eventEmitter.off('narrowChanged', handlenarrowChanged);
-      eventEmitter.off('hassChanged', handlehassChanged);
+      eventEmitter.off("narrowChanged", handlenarrowChanged);
+      eventEmitter.off("hassChanged", handlehassChanged);
     };
   }, [narrowRef.current, ref.current]);
 
+
+  function changeWeek(arg0: number) {
+    if(cwDate.cw + arg0 > 52) {
+    setCwDate({cw: 1, year: cwDate.year + 1}); cwDateChanged({cw: 1, year: cwDate.year + 1});
+    }
+    else if(cwDate.cw + arg0 < 1) {
+      setCwDate({cw: 52, year: cwDate.year - 1}); cwDateChanged({cw: 52, year: cwDate.year - 1});
+    }
+    else
+    {
+    setCwDate({cw: cwDate.cw + arg0, year: cwDate.year}); cwDateChanged({cw: cwDate.cw + arg0, year: cwDate.year});
+    }
+  }
+
   return (
-      <><AppBar position="fixed" style={{ width: "calc(100% - var(--mdc-drawer-width, 0px))", background: "#101e24", zIndex:199 }}>
-      <Toolbar style={{ minHeight: "55px" }}>
-          <ha-menu-button
-            slot="navigationIcon" 
-            ref={ref}
-          />
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontSize: "17.5px", fontWeight: 400 }}>
-        MealWish
-      </Typography>
-      <div>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          onClick={handleMenu}
-          color="inherit"
-        >
-          <MoreIcon />
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-        </Menu>
-      </div>
-    </Toolbar><Divider />
-      </AppBar></>
+    <>
+      <AppBar
+        position="fixed"
+        style={{
+          width: "calc(100% - var(--mdc-drawer-width, 0px))",
+          background: "#101e24",
+          zIndex: 199,
+        }}
+      >
+        <Toolbar style={{ minHeight: "55px" }}>
+          <ha-menu-button slot="navigationIcon" ref={ref} />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontSize: "17.5px", fontWeight: 400 }}
+          >
+            MealWish
+          </Typography>
+          <Box sx={{ flexGrow: 1, alignItems: "center",  display: { xs: "flex" } }}>
+             <IconButton
+              size="large"
+              onClick={() => {changeWeek(-1)}}
+              color="inherit"
+              >            
+              <ArrowBackIosIcon />
+            </IconButton>
+            <Typography
+              variant="button"
+            >
+              KW {String(cwDate.cw).padStart(2, '0')} {cwDate.year}
+            </Typography>
+             <IconButton
+              size="large"
+              onClick={() => {changeWeek(+1)}}
+              color="inherit"
+              >
+                <ArrowForwardIosIcon />
+              </IconButton>
+          </Box>
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+        <Divider />
+      </AppBar>
+    </>
   );
 }
