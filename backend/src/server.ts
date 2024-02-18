c68fa42212462f3ef112bde53ce0f2be9e278886
Maurice Schmicking <17197791+0x7878@users.noreply.github.com>
2024-02-18 23:08:52 +0000

    Add start:dev and start:prod scripts, update file upload path

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,37 +2,47 @@ import multer from 'multer';
 import express from 'express';
 import path from 'path';
 import jsonServer from 'json-server';
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+
+const env = process.env.NODE_ENV || 'development';
+console.log('env:', env);
+const addonConfigPath = '/config';
+const localPath = '.'; 
+
+const dataPath = env === 'production' ? addonConfigPath : localPath;
+console.log('dataPath:', dataPath);
+
+const server = jsonServer.create();
+const router = jsonServer.router(dataPath + '/db.json');
+const middlewares = jsonServer.defaults();
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, dataPath + '/uploads'); 
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) // append the original file extension
+    cb(null, Date.now() + path.extname(file.originalname));
   }
-})
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-server.use(middlewares)
-
-
+server.set('trust proxy', true);
+server.use(middlewares);
 // Set up a route for file uploads
 server.post('/upload', upload.single('image'), (req, res) => {
-  //check if file is present
+  // Check if file is present
   if (!req.file) {
     return res.send(JSON.stringify({error: 'No file uploaded'}));
   }
-  //send path of the file
+  // Send path of the file
   res.send(JSON.stringify({path: "static/" + req.file.filename}));
 });
 
-server.use('/static', express.static(path.join(__dirname, "../", 'uploads')));
-
-server.use(router)
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+// Serve static files from the uploads directory
+server.use('/static', express.static(path.join(dataPath, 'uploads')));
+const port = process.env.PORT || 3000;
+server.use(router);
+server.listen(port, () => {
+  console.log('JSON Server is running on port ' + port);
+});
